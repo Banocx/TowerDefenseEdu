8c258afe3f9da81d0f2c5b7327fef48f819c7327
Edu Botero <banocx@gmail.com>
2024-01-23 06:28:25 -0500

    network VR develop

---
+++ Assets/IRONHEADGames/Scripts/ButtonInteraction.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class ButtonInteraction : MonoBehaviour, IInteractiveUI
+{
+   
+
+    public UnityEvent OnClicked;
+
+    [SerializeField] private Material mainMaterial;
+    [SerializeField] private Material highlightedMaterial;
+
+    private Renderer _renderer;
+
+    private bool _isHighlighted = false;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _renderer.material = mainMaterial;
+    }
+
+    public void HighlightUIObject()
+    {
+        if (!_isHighlighted)
+        {
+            _renderer.material = highlightedMaterial;
+            Debug.Log("Highlighted");
+            _isHighlighted = true;
+        }
+        else
+        {
+            _renderer.material = mainMaterial;
+            Debug.Log("Highlighted off");
+            _isHighlighted = false;
+        }
+    }
+
+    public UnityEvent GetUnityEvent()
+    {
+        return OnClicked;
+    }
+
+
+}
+public interface IInteractiveUI
+{
+
+    public void HighlightUIObject();
+
+    public UnityEvent GetUnityEvent();
+}
+
